14346e1bcf2fd96e71bfaef9f12cea6163ac1a5d
ferriplatypus <55044083+ferriplatypus@users.noreply.github.com>
2021-06-27 05:14:25 +0700

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.3.1',
+      'version' => '4.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8fa0d3010112dca5dd6dd501173415856001ba8b',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.3.1',
+      ),
+    ),
+  ),
+);
